6b667424e8378b0dd6cf7a2984a634205c31e4f2
Anneli Sepp <annelisepp@gmail.com>
2025-11-26 00:54:55 +0200

    Final version – Kindergarten and Cocktails Selenium tests

---
+++ SeleniumShopUITests/CocktailsNavigationTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+
+namespace SeleniumShopUITests
+{
+    [TestClass]
+    public class CocktailsNavigationTests : BaseTest
+    {
+        [TestMethod]
+        public void CanNavigateToCocktailsIndex()
+        {
+            // Home
+            driver.Navigate().GoToUrl(BaseUrl);
+
+            // Menu
+            driver.FindElement(By.LinkText("Cocktails")).Click();
+
+            StringAssert.Contains(driver.Url, "/Cocktails");
+        }
+    }
+}
---
+++ SeleniumShopUITests/CocktailsSearchTests.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+
+namespace SeleniumShopUITests
+{
+    [TestClass]
+    public class CocktailsSearchTests : BaseTest
+    {
+        [TestMethod]
+        public void CanSearchValidCocktail()
+        {
+            driver.Navigate().GoToUrl(BaseUrl);
+
+
+            driver.FindElement(By.LinkText("Cocktails")).Click();
+
+   
+            var searchBox = driver.FindElement(By.Id("SearchCocktail"));
+            searchBox.Clear();
+            searchBox.SendKeys("Martini");
+
+            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+            var searchButton = wait.Until(d =>
+                d.FindElement(By.CssSelector("button[type='submit'], input[type='submit']")));
+            searchButton.Click();
+
+
+            StringAssert.Contains(driver.Url, "/Cocktails");
+
+
+            var bodyText = driver.FindElement(By.TagName("body")).Text;
+            StringAssert.Contains(bodyText, "Martini");
+        }
+
+        [TestMethod]
+        public void CannotSearchInvalidCocktail()
+        {
+            driver.Navigate().GoToUrl(BaseUrl);
+
+            driver.FindElement(By.LinkText("Cocktails")).Click();
+
+            var searchBox = driver.FindElement(By.Id("SearchCocktail"));
+            searchBox.Clear();
+            searchBox.SendKeys("EiOleSellistKokteili123");
+
+            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+            var searchButton = wait.Until(d =>
+                d.FindElement(By.CssSelector("button[type='submit'], input[type='submit']")));
+            searchButton.Click();
+
+ 
+            StringAssert.Contains(driver.Url, "/Cocktails");
+
+
+            var bodyText = driver.FindElement(By.TagName("body")).Text;
+            Assert.IsFalse(bodyText.Contains("EiOleSellistKokteili123"));
+        }
+    }
+}
